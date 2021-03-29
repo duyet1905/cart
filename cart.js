c16ebe4a8e4f5eff2988f01228d85225212437d5
Thế Duyệt Phạm <duyet1905@gmail.com>
2021-03-29 16:31:27 +0700

    first commit

---
+++ cart.js
@@ -0,0 +1,12 @@
+"Apple": [
+				"Iphone X", "Iphone XS", "Iphone 11", "Iphone 12", "Iphone 12 pro"
+			],
+			"Sam Sung": [
+				"Galaxy A11", "Galaxy Note 10", "Galaxy Note 8", "Galaxy Note s8"
+			],
+			"Xiaomi": [
+				"Redmi Note 8", "Redmi Note 9", "Redmi Note 9s"
+			],
+            "Oppo": [
+				"F11", "Neo9", "A31"
+			]
